a3b5cadb5a5ef464fd022649c976bf767c108d53
JKanaiya <JKanaiya>
2025-11-30 19:48:31 +0300

    Style: First Draft

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,49 @@
+import about from "../Styles/about.module.css";
+import { SiGithub, SiLinkedin } from "react-icons/si";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoMdDownload } from "react-icons/io";
+
 function About() {
-  return <div></div>;
+  return (
+    <div className={about.body}>
+      <div className={about.profile}>
+        <img
+          src="../../public/assets/Images/PPS_1242(1).jpg"
+          alt="Jonathan Kanaiya"
+        />
+        <div>
+          <span className={about.iconLinks}>
+            <a href="https://github.com/JKanaiya">
+              <SiGithub />
+              https://github.com/JKanaiya
+            </a>
+            <a href="https://www.linkedin.com/in/jonathan-kanaiya-00a4b1332/">
+              <SiLinkedin />
+              https://www.linkedin.com/in/jonathan-kanaiya
+            </a>
+            <a href="mailto:jonathankanaiya@gmail.com">
+              <MdOutlineEmail /> jonathankanaiya@gmail.com
+            </a>
+            <a
+              href="../../public/assets/jonathan-kanaiya-resume.pdf"
+              download="Jonathan Kanaiya Resume"
+            >
+              <IoMdDownload />
+              Download Resume
+            </a>
+          </span>
+        </div>
+      </div>
+      <aside>
+        <h1>About Me</h1>
+        <p>
+          I am a Full Stack Engineer with experience building websites and web
+          applications. I specialize in React, Express, PostgreSQL and Node, and
+          have experience building applications with Flutter, Dart, C# and Java.
+        </p>
+      </aside>
+    </div>
+  );
 }
 
 export default About;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,16 @@
 import projects from "../projects.json";
-import { SiGithub, SiReact } from "react-icons/si";
+import p from "../Styles/project.module.css";
+import { RxArrowTopRight } from "react-icons/rx";
+import {
+  SiGithub,
+  SiReact,
+  SiNodedotjs,
+  SiPassport,
+  SiPrisma,
+  SiPostgresql,
+  SiExpress,
+  SiAxios,
+} from "react-icons/si";
 
 const icons = {
   React: <SiReact />,
@@ -22,32 +33,41 @@ function Project({
   livelink = "",
 }) {
   return (
-    <div>
-      <div>
-        {even & <img src={`${image}`} alt={title} />}
-        <div>
+    <div key={desc} className={p.project}>
+      {even && <img src={`${image}`} alt={title} />}
+      <div className={p.article}>
+        <div className={p.head}>
+          <h3>{title}</h3>
           <div>
-            <h3>{title}</h3>
-            {live & <a href={livelink}></a>}
+            {live && (
+              <a href={livelink}>
+                <RxArrowTopRight />
+              </a>
+            )}
             <a href={ghlink}>
               <SiGithub />
             </a>
           </div>
-          {technologies.map((tech) => icons[tech])}
-          <p>{desc}</p>
         </div>
+        <p>{desc}</p>
+        <span className={p.technologies}>
+          {technologies.map((tech) => icons[tech])}
+        </span>
       </div>
-      {!even & <img src={`${image}`} alt={title} />}
+      {!even && <img src={`${image}`} alt={title} />}
     </div>
   );
 }
 
 function Projects() {
   return (
-    <div>
-      {projects.map((proj) => (
+    <div className={p.container}>
+      {projects.projects.map((proj) => (
         <Project
-          even={projects.indexOf(proj) % 2 == 0}
+          even={
+            projects.projects.indexOf(proj) % 2 == 0 ||
+            projects.projects.indexOf(proj) == 0
+          }
           title={proj.title}
           desc={proj.desc}
           image={proj.image}
