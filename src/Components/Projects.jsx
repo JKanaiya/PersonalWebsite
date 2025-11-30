43b5414fe40ed1ee5dc393ffaf0ee9c22325dc60
JKanaiya <JKanaiya>
2025-11-30 17:20:07 +0300

    Feat: projects.json + integration with images folder

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,64 @@
+import projects from "../projects.json";
+import { SiGithub, SiReact } from "react-icons/si";
+
+const icons = {
+  React: <SiReact />,
+  Axios: <SiAxios />,
+  Nodedotjs: <SiNodedotjs />,
+  Postgresql: <SiPostgresql />,
+  Prisma: <SiPrisma />,
+  Passport: <SiPassport />,
+  Express: <SiExpress />,
+};
+
+function Project({
+  even,
+  title,
+  desc,
+  image,
+  technologies,
+  ghlink,
+  live,
+  livelink = "",
+}) {
+  return (
+    <div>
+      <div>
+        {even & <img src={`${image}`} alt={title} />}
+        <div>
+          <div>
+            <h3>{title}</h3>
+            {live & <a href={livelink}></a>}
+            <a href={ghlink}>
+              <SiGithub />
+            </a>
+          </div>
+          {technologies.map((tech) => icons[tech])}
+          <p>{desc}</p>
+        </div>
+      </div>
+      {!even & <img src={`${image}`} alt={title} />}
+    </div>
+  );
+}
+
 function Projects() {
-  return <div></div>;
+  return (
+    <div>
+      {projects.map((proj) => (
+        <Project
+          even={projects.indexOf(proj) % 2 == 0}
+          title={proj.title}
+          desc={proj.desc}
+          image={proj.image}
+          technologies={proj.technologies}
+          ghlink={proj.ghlink}
+          live={proj.live}
+          livelink={proj.livelink}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Projects;
